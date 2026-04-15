54dfd58973afd267b616cc95207536c956d331e2
Tomiris0610 <t.arzanbek0610@gmail.com>
2026-04-15 22:10:31 +0500

    Lab1

---
+++ Lab1/#Задание1.py
@@ -0,0 +1,14 @@
+#Задание1
+# Ввод данных
+a = int(input("Введите первое число: "))
+b = int(input("Введите второе число: "))
+
+# Вычисления
+sum_result = a + b
+diff_result = a - b
+prod_result = a * b
+
+# Вывод результатов
+print("Сумма:", sum_result)
+print("Разность:", diff_result)
+print("Произведение:", prod_result)
